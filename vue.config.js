32e9a3768f498a72635f5edb0d0e3a026dfbd1d4
Thomas Huber <me@tohuber.com>
2021-09-26 18:22:40 -0700

    what

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  devServer: { proxy: "https://typio.github.io/" },
+  devServer: { proxy: "http://localhost:8080" },
   publicPath: process.env.NODE_ENV === "production" ? "" : "/",
 };
