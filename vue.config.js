0e0e335974a262df226f7f1dbc7b2f78fe4639eb
Thomas Huber <me@tohuber.com>
2021-09-27 13:06:49 -0700

    build

--- vue.config.js
+++ vue.config.js
@@ -17,5 +17,5 @@ module.exports = {
       },
     },
   },
-  publicPath: process.env.NODE_ENV === "production" ? "" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/music-browser/" : "/",
 };
