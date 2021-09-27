08f94b3231723992ec98c84a34fd32e440040660
Thomas Huber <me@tohuber.com>
2021-09-26 18:29:04 -0700

    cors

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,21 @@
 module.exports = {
-  devServer: { proxy: "http://localhost:8080" },
+  devServer: {
+    proxy: {
+      "/V1": {
+        target: "http://localhost:8080",
+        changeOrigin: true,
+        pathRewrite: {
+          "^/V1": "",
+        },
+      },
+      "/V2": {
+        target: "https://typio.github.io/music-browser/",
+        changeOrigin: true,
+        pathRewrite: {
+          "^/V2": "",
+        },
+      },
+    },
+  },
   publicPath: process.env.NODE_ENV === "production" ? "" : "/",
 };
